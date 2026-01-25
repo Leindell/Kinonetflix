9001d3c2c1bf10cdeef69f46a357403f55b58f48
Leindell <nikitabelov724@gmail.com>
2026-01-25 15:10:24 +0300

    баг фикс избранного

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Layout from './components/Layout.jsx'
-import CatalogPage from './pages/CatalogPage.jsx'
-import MoviePage from './pages/MoviePage.jsx'
-import NotFoundPage from './pages/NotFoundPage.jsx'
+import Layout from "./components/Layout";
+import CatalogPage from "./pages/CatalogPage";
+import MoviePage from "./pages/MoviePage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Navigate to="/movies" replace />} />
-
         <Route path="/movies" element={<CatalogPage contentType="movie" />} />
         <Route path="/series" element={<CatalogPage contentType="series" />} />
         <Route path="/anime" element={<CatalogPage contentType="anime" />} />
-
-        <Route path="/content/:id" element={<MoviePage />} />
-
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/movie/:id" element={<MoviePage />} />
+        <Route path="*" element={<div style={{ padding: 16 }}>404</div>} />
       </Route>
     </Routes>
-  )
+  );
 }
